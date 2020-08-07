438798c0e1cc847d28b1f4dde8c85c3263564c3a
unknown <barrste1@gmail.com>
2020-08-07 13:25:02 -0400

    scaffold

--- Program.cs
+++ Program.cs
@@ -7,26 +7,6 @@ namespace midtermproj
         static void Main(string[] args)
         {
 
-
-
-            Console.WriteLine("Once upon a time there was a dragon named Albi");
-            Console.WriteLine("and he was super racist");
-            while (true)
-            {
-                Console.Beep();
-                Console.Beep();
-                Console.Beep();
-                Console.Beep();
-                Console.Beep();
-
-                Console.WriteLine("so everyone kicked him out of the village and racism was solved forever.");
-
-                Console.WriteLine("bruh");
-                Console.WriteLine("You'll never change me");
-            }
-
-
-
         }
     }
 }
