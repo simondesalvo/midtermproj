3a7228aa3f40433ff55479fdcfcaf79815a14233
Curtis Godzisz <curtisgodzisz@gmail.com>
2020-08-07 11:01:56 -0400

    Mine

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace midtermproj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("You'll never change me");
         }
     }
 }
