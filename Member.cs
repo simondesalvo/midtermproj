bf4cae6b280b19921666df83ea1e10d5e22697cb
unknown <barrste1@gmail.com>
2020-08-07 14:19:10 -0400

    abstract method added

--- Member.cs
+++ Member.cs
@@ -6,6 +6,17 @@ namespace midtermproj
 {
     abstract class Member
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Member()
+        {
+
+        }
+        public Member(int id, string name)
+        {
+            id = Id; name = Name;
+        }
+        abstract public void CheckIn(ClubClass club);
 
     }
 }
