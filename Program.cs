f4793454f07a4cd708636f2b506237e1c5718449
simondesalvo <simondesalvo@gmail.com>
2020-08-07 10:58:04 -0400

    merging error test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace midtermproj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wow are Simon and Joe related?");
         }
     }
 }
