bcb8cf7d3f69f892bba1eacfc5ae0f26f3239cbf
unknown <barrste1@gmail.com>
2020-08-07 14:03:10 -0400

    Added some validation and utility classes

--- Utility.cs
+++ Utility.cs
@@ -6,5 +6,50 @@ namespace midtermproj
 {
     class Utility
     {
+        public static bool LoopProgram(string message)
+        {
+            bool end = false;
+            string cont = "";
+            Console.WriteLine(message);
+            while (cont.ToLower() != "n")
+            {
+                cont = Console.ReadLine().ToLower();
+                if (cont == "y")
+                {
+                    end = false;
+                    break;
+                }
+                else if (cont == "n")
+                {
+                    end = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter (y) to continue and (n) to stop.");
+                }
+            }
+            return end;
+        }
+        public static string GetInput(string message)
+        {
+            string input = "";
+            PrintGreen(message);
+            input = Console.ReadLine();
+            return input;
+
+        }
+        public static void PrintGreen(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public static void PrintYellow(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
     }
 }
+
--- Validate.cs
+++ Validate.cs
@@ -6,5 +6,43 @@ namespace midtermproj
 {
     class Validate
     {
+        public static int Integer(string message)
+        {
+            string input = "";
+            int intInput = -1;
+            input = Utility.GetInput(message);
+            while (int.TryParse(input,out intInput))
+            {
+                Console.Beep();
+                input = Utility.GetInput(message);
+            }
+            return = intInput;
+
+        }
+        public static bool YesNo(string message)
+        {
+            string input = "";
+            input = Utility.GetInput(message);
+            bool end = false;
+            while (!end)
+            {
+                if (input.ToLower() == "y")
+                {
+                    return !end;
+                }
+                else if (input.ToLower() == "n")
+                {
+                    Console.Clear();
+                    return end;
+                }
+                else
+                {
+                    input = Utility.GetInput("Please enter  \"y\" for yes or \"n\" for no.");
+                }
+            }
+            return end;
+        }
+
     }
+
 }
