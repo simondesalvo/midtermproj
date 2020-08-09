e88eba8a894c1dbb8d77a32b47277dbed0aeb0f9
simondesalvo <simondesalvo@gmail.com>
2020-08-09 17:22:28 -0400

    trying again

---
+++ FileIO.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace midtermproj
+{
+    class FileIO
+    {
+       
+
+        #region Members
+
+        public List<Member> MemberDbPull()
+        {
+            List<Member> members = new List<Member>();
+
+            StreamReader reader = new StreamReader("../../../MembersDb.txt");
+
+            string memberline = reader.ReadLine();
+            while (memberline != null)
+            {
+                string[] membersplit = memberline.Split('|');
+                if (membersplit.Length == 6)
+                {
+                    members.Add(new Single_Club_Member(int.Parse(membersplit[0]), membersplit[1], DateTime.Parse(membersplit[2]),
+                                                       int.Parse(membersplit[3]), bool.Parse(membersplit[4]), double.Parse(membersplit[5])));
+                    memberline = reader.ReadLine();
+                }
+                else if (membersplit.Length == 8)
+                {
+                    members.Add(new MultiClubMember(int.Parse(membersplit[0]), membersplit[1], DateTime.Parse(membersplit[2]),
+                                                    int.Parse(membersplit[3]), bool.Parse(membersplit[4]), int.Parse(membersplit[5]),
+                                                    double.Parse(membersplit[6]), int.Parse(membersplit[7])));
+                    memberline = reader.ReadLine();
+                }
+            }
+            reader.Close();
+
+            return members;
+
+        }
+
+        public void MembersDbPush()
+        {   //Make list here consistent.
+            List<Member> membersDb = new List<Member>();
+
+            StreamWriter writer = new StreamWriter("../../../MembersDb.txt");
+
+            foreach (MultiClubMember member in membersDb)
+            {
+                writer.WriteLine($"{member.ID} | {member.Name} | {member.Enroll} | {member.Club} | {member.Employee} | {member.Points} | " +
+                                 $"{member.Bill} | {member.Checkins} ");
+            }
+
+            foreach(Single_Club_Member member in membersDb)
+            {
+                writer.WriteLine($"{member.ID} | {member.Name} | {member.Enroll} | {member.Club} | {member.Employee} | {member.Bill} ");
+            }
+            writer.Close();
+        }
+        #endregion
+
+        #region Club
+
+        public List<ClubClass> ClubsDbPull()
+        {
+            List<ClubClass> clubs = new List<ClubClass>();
+
+            StreamReader reader = new StreamReader("../../../ClubDb.txt");
+
+            string clubline = reader.ReadLine();
+            while (clubline != null)
+            {
+                string[] clubsplit = clubline.Split('|');
+                clubs.Add(new ClubClass(clubsplit[0], clubsplit[1], (Ci)Enum.Parse(typeof(Ci),
+                                        clubsplit[2]), (St)Enum.Parse(typeof(St), clubsplit[3])));
+                clubline = reader.ReadLine();
+            }
+            reader.Close();
+
+            return clubs;
+        }
+
+        public void ClubsDbPush()
+        {
+            List<ClubClass> ClubDb = ClubClass.ListClubs();
+
+            StreamWriter writer = new StreamWriter("../../../ClubDb.txt");
+
+            foreach (ClubClass club in ClubDb)
+            {
+                writer.WriteLine($"{club.ClubName} | {club.StreetAddress} | {club.City} | {club.State}");
+            }
+            writer.Close();
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,91 +12,96 @@ namespace midtermproj
     {
         static void Main(string[] args)
         {
-            bool end = false;
-            while (!end)
-            {
-                Menu.DisplayMainMenu();
+            FileIO IO = new FileIO();
 
-                #region AddMember
-                /*    1)Add member;
-                //            What is  members name? ________________; name goes into name property of member object Variable for now.
-
-                check DB; if they are there, say already a member! (probably if() conditional; bounce to main menu)
-
-                                Single or Multigym? 
-
-                                    --->new member(); new member object created, goes on list of members                
+            IO.ClubsDbPull();
 
 
-                                                                SINGLE STUFF
-                                                                "What gym do they want to join?"
-                //                  member.IDassign()  -> # Xxx     X = gym
-                                                                    xx = how many registered to that gym
+            //bool end = false;
+            //while (!end)
+            //{
+            //    Menu.DisplayMainMenu();
+
+            //    #region AddMember
+            //    /*    1)Add member;
+            //    //            What is  members name? ________________; name goes into name property of member object Variable for now.
+
+            //    check DB; if they are there, say already a member! (probably if() conditional; bounce to main menu)
+
+            //                    Single or Multigym? 
+
+            //                        --->new member(); new member object created, goes on list of members                
 
 
-                                                                   Multistuff
-                                      member.IDassign() 
-
-                                                                    Display Info
-                                                            "Does this look correct?"
-                                        If Yes,
-                                            After object is filled in, add it to the file IO
-                                        If No,
-
-                */
-                #endregion
-
-                #region Display Info
-                /*
-                            Prompt user: Who are you looking up?
-                            Scan list of ALL members
-                            membername.DiplayInfo();
-                            press ket to return to main menu. Clear console and resets main menu
-                 */
-                #endregion
-
-                #region Remove Member
-                /*
-                        Prompt user: Who do you want to remove?
-                        Input name, search list
-                        name.DisplayInfo()
-                        "Are you sure you wish to delete this person?"
-                            (Y/N)
-                        if Y, delete from list (using index). This will remove from database as it is rewriten to file IO
-                        if N, the return to main menu
-                        Option: return to remove member instead
-
-                                   */
-                #endregion
-
-                #region  Fees / Points
-                /*
-                    Prmopt user for "Please input member Name"
-                                    Search list of ALL members
-                                conditional: 
-                                        if (employee is true)
-                                        {
-                                                discount fees for both (fee * 0.9)
-                                         }
-                                            if multimember (Show Fees and points earned)
-                                             else if single member (show fees)
-                            */
-                #endregion
-
-                #region Quit
-                /*
-
-                ends loop and closes things
-                maybe upload to file IO when closing        
+            //                                                    SINGLE STUFF
+            //                                                    "What gym do they want to join?"
+            //    //                  member.IDassign()  -> # Xxx     X = gym
+            //                                                        xx = how many registered to that gym
 
 
-                */
-                end = Validate.YesNo("Are you sure you want to quit?");
-                #endregion
-            }
-            Console.Clear();
-            Console.Beep(800,200); Console.Beep(400, 200); Console.Beep(200, 200); Console.Beep(100, 200);
-            Utility.PrintGreen("Thank you for using the IHeartDiamond: Clubs manager! Have a nice day!");
+            //                                                       Multistuff
+            //                          member.IDassign() 
+
+            //                                                        Display Info
+            //                                                "Does this look correct?"
+            //                            If Yes,
+            //                                After object is filled in, add it to the file IO
+            //                            If No,
+
+            //    */
+            //    #endregion
+
+            //    #region Display Info
+            //    /*
+            //                Prompt user: Who are you looking up?
+            //                Scan list of ALL members
+            //                membername.DiplayInfo();
+            //                press ket to return to main menu. Clear console and resets main menu
+            //     */
+            //    #endregion
+
+            //    #region Remove Member
+            //    /*
+            //            Prompt user: Who do you want to remove?
+            //            Input name, search list
+            //            name.DisplayInfo()
+            //            "Are you sure you wish to delete this person?"
+            //                (Y/N)
+            //            if Y, delete from list (using index). This will remove from database as it is rewriten to file IO
+            //            if N, the return to main menu
+            //            Option: return to remove member instead
+
+            //                       */
+            //    #endregion
+
+            //    #region  Fees / Points
+            //    /*
+            //        Prmopt user for "Please input member Name"
+            //                        Search list of ALL members
+            //                    conditional: 
+            //                            if (employee is true)
+            //                            {
+            //                                    discount fees for both (fee * 0.9)
+            //                             }
+            //                                if multimember (Show Fees and points earned)
+            //                                 else if single member (show fees)
+            //                */
+            //    #endregion
+
+            //    #region Quit
+            //    /*
+
+            //    ends loop and closes things
+            //    maybe upload to file IO when closing        
+
+
+            //    */
+            //    end = Validate.YesNo("Are you sure you want to quit?");
+            //    #endregion
+            //}
+            //Console.Clear();
+            //Console.Beep(800,200); Console.Beep(400, 200); Console.Beep(200, 200); Console.Beep(100, 200);
+            //Utility.PrintGreen("Thank you for using the IHeartDiamond: Clubs manager! Have a nice day!");
         }
     }
 }
